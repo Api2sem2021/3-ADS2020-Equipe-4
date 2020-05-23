932029e9251f56b0efc033ccf73b46065428cda1
angelovlima <angelovictor2001@hotmail.com>
2020-05-22 21:21:49 -0300

    Teste

---
+++ esw backend/src/test/java/com/esw/EswBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EswBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
