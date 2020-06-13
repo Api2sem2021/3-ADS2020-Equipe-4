a12977704c39f0058168e340d9656c3473b60e01
Isabella Peixoto <bsegundo2001@gmail.com>
2020-06-13 20:41:00 -0300

    commit javascript post cadastro inicial

---
+++ esw_frontend/bootstrap-3.3.7-dist/js/cadastroCandidato.js
@@ -0,0 +1,30 @@
+
+function cadastrar(){
+    var nome = $("#nome").val();
+    var cpf = $("#cpf").val();
+    var email = $("#email").val();
+    var telefone = $("#telefone").val();
+    var curriculo = $("#curriculo").val();
+
+    $.ajax({
+        type: "POST",
+        url: "~/esw_backend/src/main/java/com/fatec/esw/resources/CandidatoResource.java/cadastrar",
+        data: {
+            id: 0,
+            nome: nome,
+            cpf: cpf,
+            email: email,
+            telefone: telefone,
+            curriculo: curriculo
+        },
+        success: function (msg) {
+            if (msg) {
+                alert("Cadastrado com sucesso!");
+                location.reload(true);
+            } else {
+                alert("Não foi possível cadastrar!");
+            }
+        },
+        dataType: "JSON"
+      });
+}
